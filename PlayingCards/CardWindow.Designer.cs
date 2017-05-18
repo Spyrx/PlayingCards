aca5bd00d055e999c37dc418e3c602f3d70f2320
Jeff <jkillius261@gmail.com>
2017-05-17 19:42:41 -0700

    Fixed window size issues on other computers. Updated Colors.

--- PlayingCards/CardWindow.Designer.cs
+++ PlayingCards/CardWindow.Designer.cs
@@ -35,41 +35,46 @@
             // 
             // btnShuffle
             // 
-            this.btnShuffle.Location = new System.Drawing.Point(439, 427);
+            this.btnShuffle.BackColor = System.Drawing.Color.LightGray;
+            this.btnShuffle.Location = new System.Drawing.Point(439, 420);
             this.btnShuffle.Name = "btnShuffle";
             this.btnShuffle.Size = new System.Drawing.Size(75, 23);
             this.btnShuffle.TabIndex = 39;
             this.btnShuffle.Text = "Shuffle";
-            this.btnShuffle.UseVisualStyleBackColor = true;
+            this.btnShuffle.UseVisualStyleBackColor = false;
             this.btnShuffle.Click += new System.EventHandler(this.btnShuffle_Click);
             // 
             // btnSort
             // 
-            this.btnSort.Location = new System.Drawing.Point(520, 427);
+            this.btnSort.BackColor = System.Drawing.Color.LightGray;
+            this.btnSort.Location = new System.Drawing.Point(520, 420);
             this.btnSort.Name = "btnSort";
             this.btnSort.Size = new System.Drawing.Size(75, 23);
             this.btnSort.TabIndex = 40;
             this.btnSort.Text = "Sort";
-            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.UseVisualStyleBackColor = false;
             this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
             // 
             // flowLayoutPanel1
             // 
             this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(1044, 397);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(995, 397);
             this.flowLayoutPanel1.TabIndex = 41;
             // 
             // CardWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1068, 457);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.ForestGreen;
+            this.ClientSize = new System.Drawing.Size(1014, 457);
             this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.btnSort);
             this.Controls.Add(this.btnShuffle);
+            this.MaximumSize = new System.Drawing.Size(1030, 496);
+            this.MinimumSize = new System.Drawing.Size(1030, 496);
             this.Name = "CardWindow";
-            this.Text = "MainWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Playing Cards";
             this.Load += new System.EventHandler(this.MainWindow_Load);
             this.ResumeLayout(false);
 
